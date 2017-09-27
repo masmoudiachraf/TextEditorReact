2faaf8dfc657fd307d59eb3041d427a1217f99c4
Jean-Marc Arsan <arsan.jm@gmail.com>
2017-09-27 16:13:14 -0400

    Fixed webview display problems

--- index.android.js
+++ index.android.js
@@ -4,27 +4,19 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View, 
-  WebView
-} from 'react-native';
+import React, { Component } from "react";
+import { AppRegistry, StyleSheet, Text, View, WebView } from "react-native";
 
 export default class TextEditor extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>  
-        <WebView
-        source={{uri: 'https://192.168.1.4:3000'}}
-        style={{marginTop: 20, height: 1000}}
-      />
-
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <View style={styles.webviewContainer}>
+          <WebView
+            source={{ uri: "https://www.google.com" }}
+          />
+        </View>
       </View>
     );
   }
@@ -33,20 +25,25 @@ export default class TextEditor extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
+    flexDirection: "column"
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    textAlign: "center",
+    margin: 10
+  },
+  webviewContainer: {
+    flex: 10,
+    marginTop: 20,
+    width: '100%'
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
 });
 
-AppRegistry.registerComponent('TextEditor', () => TextEditor);
+AppRegistry.registerComponent("TextEditor", () => TextEditor);
